3b47078219e6388250cf96c1418ecf8a5c28ee0b
ramon-richard <ramon.richard@iisgalvanimi.edu.it>
2022-04-04 08:46:05 +0000

    correggere

--- improving-http/src/app/foo/foo.component.ts
+++ improving-http/src/app/foo/foo.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/internal/Observable';
+import {Foo} from './foo.model';
 
 @Component({
   selector: 'app-foo',
@@ -8,57 +9,62 @@ import { Observable } from 'rxjs/internal/Observable';
   styleUrls: ['./foo.component.css']
 })
 export class FooComponent implements OnInit {
-  data: Object | undefined;
-   loading : boolean = false;
-   o :Observable<Object> | undefined;
-
+  fooData : Foo[];
+  data: Object;
+  loading: boolean;
+  o: Observable<Object>;
+  oFoo : Observable<Foo[]>;
   constructor(public http: HttpClient) { }
+
   makeRequest(): void {
-    console.log("here");
+    
     this.loading = true;
     this.o = this.http.get('https://jsonplaceholder.typicode.com/posts/1');
     this.o.subscribe(this.getData);
   }
-  getData = (d : Object) =>
-  {
-    this.data = new Object(d);
+
+  getData = (d: Object) => {
+    this.data = d;
     this.loading = false;
   }
-  //Nota bene, questo è un metodo alternativo e compatto per fare la stessa cosa di 
-  //makeRequest senza dichiarare la variabile Observable e creando l’arrow function   
-  //direttamente dentro il metodo subscribe
+
+  //Nota bene, questo è un metodo alternativo al metodo makeRequest
   makeCompactRequest(): void {
     this.loading = true;
     this.http
       .get('https://jsonplaceholder.typicode.com/posts/1')
-      .subscribe(newData => {
-      this.data = newData;
-      this.loading = false;
+      .subscribe(data => {
+        this.data = data;
+        this.loading = false;
       });
-     }
+  }
 
-     
-      //L'operazione di post necessita un parametro in più.
-//Viene creata una stringa (JSON.strigify) a partire da un oggetto Typescript
+  //L'operazione di post necessita un parametro in più.
+  //Viene creata una stringa (JSON.strigify) a partire da un oggetto Typescript
+  makeCompactPost(): void {
+    this.loading = true;
+    this.http
+      .post('https://jsonplaceholder.typicode.com/posts',
+        JSON.stringify({ 
+          body: 'bar',
+          title: 'foo',
+          userId: 1
+        })
+      )
+      .subscribe(data => {
+        this.data = data;
+        this.loading = false;
+      });
+  }
 
-      makeCompactPost(): void {
-        this.loading = true;
-        this.http
-          .post('https://jsonplaceholder.typicode.com/posts',
-            JSON.stringify({
-              body: 'bar',
-              title: 'foo',
-              userId: 1
-            })
-          )
-          .subscribe(data => {
-            this.data = data;
-            this.loading = false;
-          });
-      }
-     
+  makeTypedRequest() : void
+  {
+    //oFoo : Observable<Foo[]>; va dichiarato tra gli attributi della classe 
+    this.oFoo = this.http.get<Foo[]>('https://jsonplaceholder.typicode.com/posts');
+    this.oFoo.subscribe(data => {this.fooData = data;});
+  }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
 }
